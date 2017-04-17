99c9afecbc3ef17980791cea8fcf06606b5e8558
giedrius.galminas <galmis@bitbucket.org>
2017-04-17 15:19:08 +0100

    playing around with server stuff

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+// NOTE: Not finished!
+
+import http from 'http';
+import path from 'path';
+import fs from 'fs';
+import express from 'express';
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+import { RouterContext, match } from 'react-router';
--- src/index.js
+++ src/index.js
@@ -14,12 +14,22 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-flatly.min.css';
 import './index.css';
 
+//
+// Grab the state from a global variable injected into the server-generated HTML
+// const preloadedState = window.__PRELOADED_STATE__;
+//
+// // Allow the passed state to be garbage-collected
+// delete window.__PRELOADED_STATE__;
+
 const rootReducer = combineReducers({
   ...reducers,
   routing: routerReducer
 });
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-const store = createStoreWithMiddleware(rootReducer);
+//const createStoreWithMiddleware = applyMiddleware()(createStore);
+//const store = createStoreWithMiddleware(rootReducer, preloadedState);
+// const store = createStore(rootReducer, preloadedState, applyMiddleware());
+
+const store = createStore(rootReducer, applyMiddleware());
 const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
