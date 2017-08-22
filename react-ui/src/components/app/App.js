055614890dd05ffa0f40637a5f24af13581bf409
giedrius.galminas <galmis@bitbucket.org>
2017-08-22 08:48:00 +0100

    trying to fix google crawler not rendering the main page

--- react-ui/src/components/app/App.js
+++ react-ui/src/components/app/App.js
@@ -2,6 +2,10 @@
 
 import React, { Component } from 'react';
 
+// It turns out that the Google Bot uses a different version of V8 from
+// the latest Chrome, and apparently doesn’t support the same EcmaScript feature set.
+import 'babel-polyfill';
+
 import { Grid } from 'react-bootstrap';
 
 import Footer from './Footer';
