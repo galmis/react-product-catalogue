68eca3f49891f0f69e65cc35184d7e7abb88094d
giedrius.galminas <galmis@bitbucket.org>
2017-08-22 08:48:36 +0100

    forgot server side changes..

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const express = require('express');
 const path = require('path');
 
@@ -8,10 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // All remaining requests return the React app, so it can handle routing.
-app.get('*', function(request, response) {
+app.get('/*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
 
 app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
+  // console.log(`Listening on port ${PORT}`);
 });
